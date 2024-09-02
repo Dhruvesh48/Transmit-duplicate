c44da99f268e2a3b41eb44823954bcf8de3e6a02
Dhruvesh48 <103595215+Dhruvesh48@users.noreply.github.com>
2024-09-02 07:34:21 +0000

    added crispy and new model community

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Community, Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
+
+
+admin.site.register(Community)
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,22 @@ from django.contrib.auth.models import User
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
+
+class Community(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_community")
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+    url = models.CharField(max_length=500, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
+    user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     content = models.TextField()
@@ -24,7 +36,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(
+    user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
